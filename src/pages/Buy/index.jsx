e0a883cf683935e9f3b07fd14c94e902b344f977
Gaurav Rana <smartgauravrana@gmail.com>
2020-04-29 21:09:12 +0530

    Buy page designed

--- src/pages/Buy/index.jsx
+++ src/pages/Buy/index.jsx
@@ -1,29 +1,121 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { Button } from "antd";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 import CustomTitle from "components/CustomTitle";
+import TextInput from "components/TextInput";
 import "./Buy.scss";
 
-export default function Buy() {
+const BuyFields = [
+  {
+    name: "transactionId",
+    type: "text",
+    placeholder: "Transaction Id"
+  },
+  {
+    name: "amount",
+    type: "number",
+    placeholder: "Amount"
+  }
+];
+
+function Buy({ userDetails }) {
+  const noticeInfo = (
+    <>
+      <CustomTitle title="Buy Chips" />
+      <p style={{ color: "indigo", fontSize: "19px" }}>
+        Pay via Paytm Wallet Only
+      </p>
+      <p className="text-danger" style={{ fontSize: "19px" }}>
+        Dont pay through Bank (You will lose your money).
+      </p>
+      <p>
+        Please pay at this number only{" "}
+        <span>
+          <b style={{ fontSize: "120%", color: "red" }}>9729343885</b>
+        </span>
+        , and enter wallet Transcation Id.
+        <br />
+        <a href="/help/loadbalance">Help</a>
+      </p>
+    </>
+  );
+
   return (
     <div className="Buy">
-      <div className="Buy__Header">
-        <CustomTitle title="Buy Chips" />
-        <p style={{ color: "indigo", fontSize: "19px" }}>
-          Pay via Paytm Wallet Only
-        </p>
-        <p className="text-danger" style={{ fontSize: "19px" }}>
-          Dont pay through Bank (You will lose your money).
-        </p>
-        <p>
-          Please pay at this number only{" "}
-          <span>
-            <b style={{ fontSize: "120%", color: "red" }}>9729343885</b>
-          </span>
-          , and enter wallet Transcation Id.
+      <div className="Buy__Header">{noticeInfo}</div>
+      <div className="Buy__paymentNumber">
+        <input
+          id="paymentNumber"
+          type="number"
+          name="paymentNumber"
+          placeholder="Recipient Paytm Number..."
+          readOnly
+          value="9729343885"
+        />
+        <div className="input-group-append">
+          <button id="copyButton">Copy</button>
+        </div>
+      </div>
+      <div className="Buy__form">
+        <Formik
+          initialValues={{
+            transactionId: "",
+            amount: ""
+          }}
+          validationSchema={Yup.object({
+            transactionId: Yup.string().required("Required!"),
+            amount: Yup.number().required("Required!")
+          })}
+          onSubmit={console.log}
+        >
+          {props => (
+            <Form>
+              {BuyFields.map(field => (
+                <TextInput key={field.name} {...field} />
+              ))}
+              <Button
+                className="Load_btn"
+                type="success"
+                onClick={() => props.isValid && props.submitForm()}
+              >
+                Load
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      <div className="Buy__QA">
+        <h3 style={{ textAlign: "center" }}>For Any Query</h3>
+        <p style={{ textAlign: "center" }}>
+          Please contact support at whatsapp (+919729343885)
           <br />
-          <a href="/help/loadbalance">Help</a>
+          Your query will be solved in <b>within 12 hours</b>.
         </p>
+        <a
+          href={`https://wa.me/919729343885?text=Please+Load+Chips+In+My+Account,+My+account+number+is+${userDetails.phone}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to contact Admin
+        </a>
       </div>
     </div>
   );
 }
+
+export default connect(
+  ({ userDetails }) => ({
+    userDetails
+  }),
+  null
+)(Buy);
+
+Buy.propTypes = {
+  userDetails: PropTypes.object,
+  isValid: PropTypes.bool,
+  submitForm: PropTypes.func
+};
