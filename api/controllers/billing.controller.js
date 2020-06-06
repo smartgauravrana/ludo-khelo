51879ad2c2909b2d2c0c5ef3a165995a6846fa84
Gaurav Rana <smartgauravrana@gmail.com>
2020-06-06 15:10:37 +0530

    billing controller tweaks

--- api/controllers/billing.controller.js
+++ api/controllers/billing.controller.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const SellRequest = mongoose.model("sellRequests");
 const { SELLING_STATUS } = require("../../constants");
 const { getMailServer, getUnseenMail } = require("../../services/mailbox");
-const { getPaytmDetails } = require("../../utils");
+const { getPaytmDetails, ErrorResponse } = require("../../utils");
+const { asyncHandler } = require("../../middlewares");
 
-module.exports.buyChips = async (req, res) => {
+// @desc      Buy Chips
+// @route     POST /api/buy
+// @access    Private
+module.exports.buyChips = asyncHandler(async (req, res, next) => {
   const { transactionId, amount } = req.body;
   const mailServer = getMailServer();
   getUnseenMail(
     { server: mailServer, data: transactionId },
     async (err, data) => {
-      if (err) return res.status(500).send({ msg: "Something went wrong!" });
+      if (err) return next(new ErrorResponse("Something went wrong!", 500));
       const { mailText, uid } = data;
       if (mailText) {
         const transaction = getPaytmDetails(mailText);
@@ -23,28 +27,34 @@ module.exports.buyChips = async (req, res) => {
           });
           req.user.chips += transaction.amount;
           const user = await req.user.save();
-          res.send(user);
+          res.send({ success: true, data: user });
         } else {
-          res.status(400).send({ msg: "Wrong details supplied!" });
+          return next(new ErrorResponse("Wrong details supplied!", 400));
         }
       } else {
-        res.status(404).send({ msg: "Transaction Id not exist!" });
+        return next(new ErrorResponse("Transaction Id not exist!", 400));
       }
     }
   );
-};
+});
 
-module.exports.getAllSellRequests = async (req, res) => {
+// @desc      GET All Sell Request
+// @route     GET /api/sell
+// @access    Private
+module.exports.getAllSellRequests = asyncHandler(async (req, res, next) => {
   const sellRequests = await SellRequest.find()
     .sort({
       _id: -1
     })
     .limit(20)
     .exec();
-  res.send(sellRequests);
-};
+  res.send({ success: true, data: sellRequests });
+});
 
-module.exports.addSellRequest = async (req, res) => {
+// @desc      Add a Sell Request
+// @route     POST /api/sell
+// @access    Private
+module.exports.addSellRequest = asyncHandler(async (req, res, next) => {
   const { amount, phone } = req.body;
   if (req.user.chips >= amount && amount >= 50) {
     await new SellRequest({
@@ -55,13 +65,19 @@ module.exports.addSellRequest = async (req, res) => {
     }).save();
     req.user.chips -= amount;
     const user = await req.user.save();
-    return res.send(user);
+    return res.send({ success: true, data: user });
   }
-  res.status(401).send({ msg: "You don't have enough chips" });
-};
+  if (amount < 50) {
+    return next(new ErrorResponse("Minimum amount is 50", 400));
+  }
+  next(new ErrorResponse("You don't have enough chips", 401));
+});
 
-module.exports.deleteSellRequest = async (req, res) => {
+// @desc      Delete a Sell Request
+// @route     DELETE /api/sell/:sellId
+// @access    Private
+module.exports.deleteSellRequest = asyncHandler(async (req, res, next) => {
   const { sellId } = req.params;
   const sellRequest = await SellRequest.findByIdAndDelete(sellId);
-  res.send(sellRequest);
-};
+  res.send({ success: true, data: sellRequest });
+});
