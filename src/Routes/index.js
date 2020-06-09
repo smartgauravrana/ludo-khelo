4414fe939a47644a9a99dfe1efa6db43d7940f9e
Gaurav Rana <smartgauravrana@gmail.com>
2020-06-09 10:05:51 +0530

    codesplitting added

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,18 +1,50 @@
 import React from "react";
+import loadable from "@loadable/component";
 import routePaths from "./routePaths";
 import SocketContext from "context/socket-context";
 
-import Login from "pages/Login";
-import Register from "pages/Register";
-import Admin from "pages/Admin";
-import Home from "pages/Home";
-import Buy from "pages/Buy";
-import Sell from "pages/Sell";
-import TermCondition from "pages/TermCondition";
+// import Login from "pages/Login";
+// import Register from "pages/Register";
+// import Admin from "pages/Admin";
+// import Home from "pages/Home";
+// import Buy from "pages/Buy";
+// import Sell from "pages/Sell";
+// import Timeline from "pages/Timeline";
+// import Manage from "pages/Manage";
+
 import MatchDetail from "pages/MatchDetail";
-import Timeline from "pages/Timeline";
-import Manage from "pages/Manage";
+import TermCondition from "pages/TermCondition";
 import Settings from "pages/Settings";
+
+const Login = loadable(() =>
+  import(/* webpackChunkName: "Login" */ "pages/Login")
+);
+
+const Register = loadable(() =>
+  import(/* webpackChunkName: "Register" */ "pages/Register")
+);
+
+const Home = loadable(() =>
+  import(/* webpackChunkName: "Home" */ "pages/Home")
+);
+
+const Buy = loadable(() => import(/* webpackChunkName: "Buy" */ "pages/Buy"));
+
+const Sell = loadable(() =>
+  import(/* webpackChunkName: "Sell" */ "pages/Sell")
+);
+
+const Timeline = loadable(() =>
+  import(/* webpackChunkName: "Timeline" */ "pages/Timeline")
+);
+
+const Admin = loadable(() =>
+  import(/* webpackChunkName: "Admin" */ "pages/Admin")
+);
+
+const Manage = loadable(() =>
+  import(/* webpackChunkName: "Managae" */ "pages/Manage")
+);
 
 export const publicRoutes = [
   {
